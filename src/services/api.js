752d8b7a972a1bc6bbe9140158ff64c538535076
marcodriano <marcosadr@outlook.com>
2025-02-12 19:29:05 -0300

    tentativa 2

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://backarvoregenealogica.onrender.com', // URL do backend
+  baseURL: 'http://localhost:5000', // URL do backend
 });
 
 // Adiciona o token ao cabeçalho das requisições
