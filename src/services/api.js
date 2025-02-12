fc4082ef1821b283ca9a9df372bde2c6d96cbd93
marcodriano <marcosadr@outlook.com>
2025-02-11 21:23:16 -0300

    deployzado

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/', // URL do backend
+  baseURL: 'https://backarvoregenealogica.onrender.com', // URL do backend
 });
 
 // Adiciona o token ao cabeçalho das requisições
